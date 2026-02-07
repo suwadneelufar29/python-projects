a20f0dcc14b8106d4a2a116d952e4279fb82c24f
suwadneelufar29 <suwadneelufarshaik@gmail.com>
2026-02-07 12:41:06 +0530

    Add Password Generator project

---
+++ password_generator.py
@@ -0,0 +1,14 @@
+# Password Generator
+
+import random
+import string
+
+length = int(input("Enter password length: "))
+
+characters = string.ascii_letters + string.digits + string.punctuation
+password = ""
+
+for _ in range(length):
+    password += random.choice(characters)
+
+print("Generated Password:", password)
